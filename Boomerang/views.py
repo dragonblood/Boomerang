7ef4381495a356b8daa0a1196a5e48ce87d211d9
Vipul <Petkar.vipul@gmail.com>
2020-06-04 17:25:06 +0530

    Charts added, bunch of style and scripts removed

--- Boomerang/views.py
+++ Boomerang/views.py
@@ -1,7 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.generic import View
 
 def Boomerang_entertext(request):
     return render(request, 'enterText.html')
 
 def Boomerang_analysis(request):
-    return render(request, 'Analysis.html')
+    #def get(self, request, *args, **kwargs):
+    return render(request, 'Analysis.html', {"hi":10})
+
+
+def get_data(request, *args, **kwargs):
+    data = {"sales":100,
+            "customers": 10,
+    }
+    return JsonResponse(data)
