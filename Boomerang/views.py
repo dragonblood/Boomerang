12bd852db7a7dfb02f15223c0b38f4540da4592b
Vipul <Petkar.vipul@gmail.com>
2020-06-19 02:37:48 +0530

    Project Done, only maintance and adding few left.

--- Boomerang/views.py
+++ Boomerang/views.py
@@ -7,30 +7,31 @@ from django.http import HttpResponse
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+import argparse
 
 sentiment = 0
 text = 0
+entity = 0
 
 def Boomerang_entertext(request):
-    api_call(request)
-    return render(request, 'enterText.html')
-
-def api_call(request):
     if request.method == 'POST':
         if request.POST.get('submittext'):
-            print('hihihih')
             global text
             text = request.POST.get('text')
-            
-            # Instantiates a client
-            client = language.LanguageServiceClient()
+            entity_api(text)
+            sentiment_api(text)
+    return render(request, 'enterText.html')
 
-            # The text to analyze
-            document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
+def sentiment_api(text):
+    # Instantiates a client
+    client = language.LanguageServiceClient()
 
-            # Detects the sentiment of the text
-            global sentiment
-            sentiment = client.analyze_sentiment(document=document)
+    # The text to analyze
+    document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    global sentiment
+    sentiment = client.analyze_sentiment(document=document)
 
 
 # def printer(text, sentence):
@@ -59,6 +60,23 @@ def Boomerang_analysis(request):
         else:
             sentimentTypeCount[0] += 1
 
-    print(sentimentTypeCount)
-    arr = [sentiment, text, sentimentTypeCount]
+    arr = [sentiment, text, sentimentTypeCount, entity]
+    print(arr)
     return render(request, 'Analysis.html', {"arr": arr})
+
+def entity_api(text):
+    document = {"content": text, "type": enums.Document.Type.PLAIN_TEXT}
+    client = language.LanguageServiceClient()
+    # Available values: NONE, UTF8, UTF16, UTF32
+    response = client.analyze_entities(document, encoding_type=enums.EncodingType.UTF8)
+
+    global entity 
+    ten = 0
+    entity = {'name':[], 'salience':[]}
+    # Loop through entitites returned from the API
+    for en in response.entities:
+        ten += 1
+        entity['name'].append(en.name)
+        entity['salience'].append(en.salience)
+        if ten == 10:
+            break
